c86933b5ea1a0d45d67d1efc89c8ffb618331629
Shavkat <shavkat.wx@gmail.com>
2023-02-26 00:32:35 +0500

    add new input

--- App/database2.php
+++ App/database2.php
@@ -12,10 +12,16 @@ class DataBase2
     }
     public function DataBaseConnect2($name)
     {
-        $param = ['name'=>$name];
+        $param = [
+            'name' => $name,
+            'surname' => $name,
+            'age' => $name,
+            'tel' => $name
+        ];
 
         $con = new PDO("mysql:host=localhost;dbname=test;charset=utf8", "root", "");
-        $sql = "INSERT test_table (name) VALUE (:name)";
+        $sql = "INSERT test_table (name,surname,age,tel) VALUE (:name,:surname,:age,:tel)";
+        
         $query = $con->prepare($sql);
         $query->execute($param);
 
